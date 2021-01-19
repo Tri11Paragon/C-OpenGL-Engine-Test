207ce89027b919b2ff57702f1c03a62b0db6ab62
Brett <brettmaster1@gmail.com>
2021-01-19 16:26:19 -0500

    test of clion commit

---
+++ src/gldata/vao.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brett on 2021-01-19.
+//
+
+#include "vao.h"
---
+++ src/gldata/vao.h
@@ -0,0 +1,15 @@
+//
+// Created by brett on 2021-01-19.
+//
+#pragma once
+
+#ifndef UNTITLED_VAO_H
+#define UNTITLED_VAO_H
+
+
+class vao {
+
+};
+
+
+#endif //UNTITLED_VAO_H
